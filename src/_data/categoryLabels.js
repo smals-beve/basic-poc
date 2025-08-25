9254185748a883e96f210fd2cb93a8774a3d80b6
Eskinder <eskinder.yegezu@smals.be>
2025-08-25 15:47:55 +0200

    Improvement

--- src/_data/categoryLabels.js
+++
@@ -1,5 +0,0 @@
-export default {
-    overview: {en: "Overview", fr: "Vue d’ensemble", nl: "Overzicht"},
-    field: {en: "Fields", fr: "Champs", nl: "Velden"},
-    block: {en: "Blocks", fr: "Blocs", nl: "Blokken"}
-};
